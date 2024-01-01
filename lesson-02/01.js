4e1e854c42ff36bdfb66c46a69d177b2f7944780
wwshadowsq <makarenkoannapublic@gmail.com>
2024-01-01 16:14:55 +0200

    changed text output for console

--- lesson-02/01.js
+++ lesson-02/01.js
@@ -1,5 +1,5 @@
 let number1 = Math.floor(Math.random() * 100);
 
 number1 % 2 === 0
-  ? console.log(number1, ":Это четное число")
-  : console.log(number1, ":Это нечетное число");
+  ? console.log(`[${number1}], :Это четное число`)
+  : console.log(`[${number1}], ":Это нечетное число`);
