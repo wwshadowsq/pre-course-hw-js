7c430705748210929dad9e51c80c02772a061f0f
wwshadowsq <makarenkoannapublic@gmail.com>
2024-01-01 16:31:06 +0200

    added if

--- lesson-02/01.js
+++ lesson-02/01.js
@@ -1,5 +1,7 @@
 let number1 = Math.floor(Math.random() * 100);
 
-number1 % 2 === 0
-  ? console.log(`${number1}, :Это четное число`)
-  : console.log(`${number1}, ":Это нечетное число`);
+if (number1 % 2 === 0) {
+  console.log(`${number1}:Это четное число`);
+} else {
+  console.log(`${number1}:Это нечетное число`);
+}
